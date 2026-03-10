6ecc92d60752ea9276d0679af7026bf6089f19ec
Soham-Sahana <sohamssahana@gmail.com>
2026-03-10 10:29:51 +0530

    Add files via upload

---
+++ BCC-1.py
@@ -0,0 +1,85 @@
+# print("Hello Students")
+# is_student = True
+# is_logged_in = False
+# age = 21
+# name = 'Soham'
+# marks = 33.3
+# print(type(is_student))
+# print(type(is_logged_in))
+# print(type(age))
+# print(type(name))
+# print(type(marks))
+
+# num = 100
+# text = str(num)
+# print(type(text))
+
+# num1 = '10'
+# num2 = '20'
+# print(num1+' '+num2)
+# print(type(num1+' '+num2))
+
+# result = int(num1) + int(num2)
+# print(result)
+
+# age = input('enter your age:')
+# print(age)
+# print(type(age))
+
+#find your current age
+# c_year = 2026
+# year = int(input("ENTER YOUR YOB:"))
+# age = c_year - year
+# print(age)
+
+#Calculate area of rectangle
+# l = float(input("Enter length:"))
+# b = float(input("Enter breadth:"))
+# area = l*b
+# print(round(area,2))
+
+#Celsius to farenheit
+# c = float(input("Enter temp in celsius:"))
+# f = (c*9/5) + 32
+# print("Temp in farenheit:",f)
+
+#Calculate simple interest
+# p = float(input('Enter principal amount:'))
+# r = float(input('Enter rate of interest:'))
+# t = float(input('Enter period of time:'))
+# SI = (p*r*t)/100
+# print(SI)
+
+#swap two numbers
+# a = 27
+# b = 11
+# a,b = b,a
+# print('a:',a)
+# print('b:',b)
+
+#find square of a number
+# n = float(input('Enter a number:'))
+# sqn = n**2
+# print(sqn)
+# cn = n**3
+# print(cn)
+
+#minutes to hours
+# min = int(input('Enter minutes:'))
+# hour = min/60
+# print("The corresponding hour is:",hour)
+
+#calculate average of 3 numbers
+# x = int(input("Enter first number:"))
+# y = int(input("Enter second number:"))
+# z = int(input("Enter third number:"))
+# avg = (x+y+z)/3
+# print(avg)
+
+# convert km to m
+# km = 22
+# m = km*1000
+# print(m)
+
+#convert rupees to dollar
+#calculate total marks and percentage
